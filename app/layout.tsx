12047966bc1da1f74f52105b57c03478c4c0fc11
CJ Starke <cjstarke@convergedirect.com>
2026-09-18 11:21:39 -0400

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "CJ Starke Portfolio",
+  title: "CJ Starke - Software Engineer",
   description: "Portfolio site for CJ Starke",
 };
 
